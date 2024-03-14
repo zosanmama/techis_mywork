c466255ae16fb19956e2996b3928d208ae8469cb
Aiko <134149662+zosanmama@users.noreply.github.com>
2024-03-14 14:45:41 +0900

    もとに戻す

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -28,13 +28,3 @@ class Handler extends ExceptionHandler
         });
     }
 }
-
-public function render($request, Exception $exception)
- {
-    // TokenMismatchException発生時
-    if ($exception instanceof TokenMismatchException) {
-        // TokenMismatchException発生時はindexへリダイレクトさせる
-        return redirect('/index');
-    }
-    return parent::render($request, $exception);
-}
