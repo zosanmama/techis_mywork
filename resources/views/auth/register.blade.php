a633ccf815afdc2edc2d6a3257f25f60e1ebd1f4
Aiko <134149662+zosanmama@users.noreply.github.com>
2024-03-14 14:39:34 +0900

    csrf token mismatch の解決

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
+                        {{ csrf_field() }}
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('氏名') }}</label>
 
