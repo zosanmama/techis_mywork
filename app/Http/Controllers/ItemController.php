4af9ba07ab3e49ffe9e74e735ddb5ca213264603
Aiko <134149662+zosanmama@users.noreply.github.com>
2024-03-20 15:47:11 +0900

    入力バリデーションを追加

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -65,9 +65,48 @@ class ItemController extends Controller
         // POSTリクエストのとき
         if ($request->isMethod('post')) {
             // バリデーション
-            $this->validate($request, [
-                'item_name' => 'required|max:100',
-            ]);
+            $rules = [
+                'item_name' => 'required|max:30',
+                'detail' => 'required|max:100',
+                'in_stock' => 'required|numeric|gt:0|digits_between:1,5',
+                'appr_inventory' => 'required|numeric|gt:0|digits_between:1,5',
+                'avr_daily_sales' => 'required|numeric|gt:0|digits_between:1,5',
+                'delivery_days' => 'required|numeric|gt:0|digits_between:1,4',
+                'supplier' => 'required|max:100',
+                'purchase_price' => 'required|numeric|gt:0|digits_between:1,7',
+            ];
+            // エラーメッセージを定義
+            $messages = [
+                'item_name.required' => '商品名を入力してください。',
+                'item_name.max' => '商品名は30文字以内で入力してください。',
+                'detail.required' => '詳細を入力してください。',
+                'detail.max' => '詳細は100文字以内で入力してください。',
+                'in_stock.required' => '在庫数を入力してください。',
+                'in_stock.numeric' => '在庫数は数値で入力してください。',
+                'in_stock.gt' => '在庫数は0より大きい値を入力してください。',
+                'in_stock.digits' => '在庫数は5桁以下の数字で入力してください。',
+                'appr_inventory.required' => '適正在庫を入力してください。',
+                'appr_inventory.numeric' => '適正在庫は数値で入力してください。',
+                'appr_inventory.gt' => '適正在庫は0より大きい値を入力してください。',
+                'appr_inventory.digits' => '承認在庫は5桁までの数字で入力してください。',
+                'avr_daily_sales.required' => '平均日次売上を入力してください。',
+                'avr_daily_sales.numeric' => '平均日次売上は数値で入力してください。',
+                'avr_daily_sales.gt' => '平均日売上は0より大きい値を入力してください。',
+                'avr_daily_sales.digits' => '平均日売上は5桁までの数字で入力してください。',
+                'delivery_days.required' => '納品日数を入力してください。',
+                'delivery_days.numeric' => '納品日数は数値で入力してください。',
+                'delivery_days.gt' => '納品日数は0より大きい値を入力してください。',
+                'delivery_days.digits' => '納品日数は4桁までの数字で入力してください。',
+                'supplier.required' => '納品先を入力してください。',
+                'supplier.max' => '納品先は100文字以内で入力してください。',
+                'purchase_price.required' => '購入価格を入力してください。',
+                'purchase_price.numeric' => '購入価格は数値で入力してください。',
+                'purchase_price.gt' => '購入価格は0より大きい値を入力してください。',
+                'purchase_price.digits' => '購入価格は7桁までの数字で入力してください。',
+            ];
+
+            // バリデーションを実行
+            $validatedData = $request->validate($rules, $messages,['ja']);
 
             // 商品登録
             Item::create([
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +24,77 @@ class AppServiceProvider extends ServiceProvider
         if (\App::environment(['production']) || \App::environment(['develop'])) {
             \URL::forceScheme('https');
         }
+
+        Validator::replacer('required', function ($message, $attribute, $rule, $parameters) {
+            $attributes = [
+                'item_name' => '商品名',
+                'detail' => '詳細',
+                'in_stock' => '在庫数',
+                'appr_inventory' => '適正在庫',
+                'avr_daily_sales' => '平均日次売上',
+                'delivery_days' => '納品日数',
+                'supplier' => '仕入れ先',
+                'purchase_price' => '仕入価格',
+            ];
+        
+            return str_replace(':attribute', $attributes[$attribute], $attributes[$attribute] . 'を入力してください。');
+        });
+        
+        Validator::replacer('max', function ($message, $attribute, $rule, $parameters) {
+            $attributes = [
+                'item_name' => '商品名',
+                'detail' => '詳細',
+                'in_stock' => '在庫数',
+                'appr_inventory' => '適正在庫',
+                'avr_daily_sales' => '平均日次売上',
+                'delivery_days' => '納品日数',
+                'supplier' => '仕入れ先',
+                'purchase_price' => '仕入価格',
+            ];
+        
+            return str_replace(':attribute', $attributes[$attribute], $attributes[$attribute] . 'は' . $parameters[0] . '文字以内で入力してください。');
+        });
+        
+        Validator::replacer('numeric', function ($message, $attribute, $rule, $parameters) {
+            $attributes = [
+                'item_name' => '商品名',
+                'detail' => '詳細',
+                'in_stock' => '在庫数',
+                'appr_inventory' => '適正在庫',
+                'avr_daily_sales' => '平均日次売上',
+                'delivery_days' => '納品日数',
+                'supplier' => '仕入れ先',
+                'purchase_price' => '仕入価格',
+            ];
+            return str_replace(':attribute', $attributes[$attribute], $attributes[$attribute] . 'は数値で入力してください。');
+        });
+        
+        Validator::replacer('gt', function ($message, $attribute, $rule, $parameters) {
+            $attributes = [
+                'item_name' => '商品名',
+                'detail' => '詳細',
+                'in_stock' => '在庫数',
+                'appr_inventory' => '適正在庫',
+                'avr_daily_sales' => '平均日次売上',
+                'delivery_days' => '納品日数',
+                'supplier' => '仕入れ先',
+                'purchase_price' => '仕入価格',
+            ];
+            return str_replace(':attribute', $attributes[$attribute], $attributes[$attribute] . 'は0より大きい値を入力してください。');
+        });
+        
+        Validator::replacer('digits_between', function ($message, $attribute, $rule, $parameters) {
+            $attributes = [
+                'item_name' => '商品名',
+                'detail' => '詳細',
+                'in_stock' => '在庫数',
+                'appr_inventory' => '適正在庫',
+                'avr_daily_sales' => '平均日次売上',
+                'delivery_days' => '納品日数',
+                'supplier' => '仕入れ先',
+                'purchase_price' => '仕入価格',
+            ];
+            return str_replace(':attribute', $attributes[$attribute], $attributes[$attribute] . 'は' . $parameters[0] . 'から' . $parameters[1] . '桁の数字で入力してください。');
+        });
     }
 }
