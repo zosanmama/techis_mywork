78915a09bb7de997ada6c1d2a13ed594b8d1aaa1
Aiko <134149662+zosanmama@users.noreply.github.com>
2024-05-13 12:52:14 +0900

    side menu update

--- config/adminlte.php
+++ config/adminlte.php
@@ -295,6 +295,11 @@ return [
             'url'  => 'items',
             'icon' => 'fas fa-gamepad',
         ],
+        [
+            'text' => '商品一覧',
+            'url'  => 'items',
+            'icon' => 'fas fa-gamepad',
+        ],
 
         // AdminLTEデフォルト
         /*
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>商品管理システム</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
